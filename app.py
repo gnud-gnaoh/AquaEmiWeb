32c8398837af5041d2353f31aa90c69ae07ed7fa
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2023-08-06 11:34:16 +0700

    Restructure files (move models and config to seperate file)

--- app.py
+++ app.py
@@ -1,58 +1,19 @@
-import datetime
 import json
 from flask import Flask, request, render_template, make_response, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import SQLAlchemySchema
 from marshmallow import fields, post_load
-from math import cos, asin, sqrt, pi
+from models import db, WaterSource, WaterSourceSchema, WaterMeasurement, WaterMeasurementSchema, WaterMeasurementApp, WaterMeasurementAppSchema
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:bubudeptrai2006@localhost:3306/bucketlist'
-app.secret_key = 'mmb'
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
-db = SQLAlchemy(app)
+db.init_app(app)
 
-### Models
-
-# WaterSource
-class WaterSource(db.Model):
-    __tablename__ = 'WaterSources'
-    id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    name = db.Column(db.String(80))
-    country = db.Column(db.String(80))
-    measurements = db.relationship('WaterMeasurement', backref='Watersource')
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-    
-    def __init__(self, latitude, longitude, name, country):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.name = name
-        self.country = country
-
-    def __repr__(self):
-        return "<WaterSource(name={self.name!r})>".format(self=self)
-
-class WaterSourceSchema(SQLAlchemySchema):
-    class Meta(SQLAlchemySchema.Meta):
-        model = WaterSource
-        sqla_session = db.session
-    
-    id = fields.Number(dump_only=True)
-    latitude = fields.Float(required=True)
-    longitude = fields.Float(required=True)
-    name = fields.String(required=True)
-    country = fields.String(required=True)
-    
-    @post_load
-    def make_water_source(self, data, **kwargs):
-        return WaterSource(**data)
-
+# would this still work?
+with app.app_context():
+    db.create_all()
 
 # water_source api
 @app.route('/water_source', methods=['GET'])
@@ -102,54 +63,6 @@ def water_source_delete_by_id(id):
     db.session.commit()
     return make_response("", 204)
     
-
-# WaterMeasurement
-class WaterMeasurement(db.Model):
-    __tablename__ = 'WaterMeasurements'
-    id = db.Column(db.Integer, primary_key=True)
-    WaterSourceid = db.Column(db.Integer, db.ForeignKey("WaterSources.id"))
-    ph = db.Column(db.Float)
-    turbidity = db.Column(db.Float)
-    conductivity = db.Column(db.Float)
-    temperature = db.Column(db.Float)
-    flow = db.Column(db.Float)
-    datetime = db.Column(db.DateTime)
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-    
-    def __init__(self, WaterSourceid, ph, turbidity, conductivity, temperature, flow):
-        self.WaterSourceid = WaterSourceid
-        self.ph = ph
-        self.turbidity = turbidity
-        self.conductivity = conductivity
-        self.temperature = temperature
-        self.flow = flow
-        self.datetime = datetime.datetime.utcnow()
-
-    def __repr__(self):
-        return "<WaterMeasurement(datetime={self.datetime!r})>".format(self=self)
-
-class WaterMeasurementSchema(SQLAlchemySchema):
-    class Meta(SQLAlchemySchema.Meta):
-        model = WaterMeasurement
-        sqla_session = db.session
-    
-    id = fields.Number(dump_only=True)
-    WaterSourceid = fields.Number(required=True)
-    ph = fields.Float(required=True)
-    turbidity = fields.Float(required=True)
-    conductivity = fields.Float(required=True)
-    temperature = fields.Float(required=True)
-    flow = fields.Float(required=True)
-    datetime = fields.DateTime(dump_only=True)
-
-    @post_load
-    def make_water_measurement(self, data, **kwargs):
-        return WaterMeasurement(**data)
-
 # water measurement api
 @app.route('/measure', methods=['GET'])
 def water_measurement_index():
@@ -202,66 +115,6 @@ def water_measurement_delete_by_id(id):
     db.session.commit()
     return make_response("", 204)
 
-
-# WaterMeasurementApp
-def distance(lat1, lon1, lat2, lon2):
-    r = 6371 # km
-    p = pi / 180
-
-    a = 0.5 - cos((lat2-lat1)*p)/2 + cos(lat1*p) * cos(lat2*p) * (1-cos((lon2-lon1)*p))/2
-    return 2 * r * asin(sqrt(a))
-
-class WaterMeasurementApp(db.Model):
-    __tablename__ = 'WaterMeasurementApps'
-    id = db.Column(db.Integer, primary_key=True)
-    latitude = db.Column(db.Float)
-    longitude = db.Column(db.Float)
-    WaterSourceid = db.Column(db.Integer, db.ForeignKey("WaterSources.id"))
-    turbidity = db.Column(db.Float)
-    color = db.Column(db.String(20))
-    datetime = db.Column(db.DateTime)
-
-    def create(self):
-        db.session.add(self)
-        db.session.commit()
-        return self
-
-    def __init__(self, latitude, longitude, turbidity, color):
-        self.latitude = latitude
-        self.longitude = longitude
-        watersources = WaterSource.query.all()
-
-        closest_watersource = watersources[0]
-        for watersource in watersources:
-            if distance(latitude, longitude, closest_watersource.latitude, closest_watersource.longitude) \
-            > distance(latitude, longitude, watersource.latitude, watersource.longitude):
-                closest_watersource = watersource
-        
-        self.WaterSourceid = closest_watersource.id
-        self.turbidity = turbidity
-        self.color = color
-        self.datetime = datetime.datetime.utcnow()
-
-    def __repr__(self):
-        return "<WaterMeasurementApp(datetime={self.datetime!r})>".format(self=self)
-
-class WaterMeasurementAppSchema(SQLAlchemySchema):
-    class Meta(SQLAlchemySchema.Meta):
-        model = WaterMeasurementApp
-        sqla_session = db.session
-    
-    id = fields.Number(dump_only=True)
-    latitude = fields.Float(required=True)
-    longitude = fields.Float(required=True)
-    WaterSourceid = fields.Number(dump_only=True)
-    turbidity = fields.Float(required=True)
-    color = fields.String(required=True)
-    datetime = fields.DateTime(dump_only=True)
-
-    @post_load
-    def make_water_measurement_app(self, data, **kwargs):
-        return WaterMeasurementApp(**data)
-
 # water measurement app api
 @app.route('/measureapp', methods=['GET'])
 def water_measurement_app_index():
@@ -310,10 +163,6 @@ def water_measurement_app_delete_by_id(id):
     db.session.commit()
     return make_response("", 204)
 
-#
-with app.app_context():
-    db.create_all()
-
 # Views
 @app.route('/', methods=['GET'])
 @app.route('/index', methods=['GET'])
@@ -358,7 +207,7 @@ def earth_page():
         "type": "FeatureCollection",
         "features": features
     }
-    
+
     return render_template('map_earth.html', data=json.dumps(data))
 
 @app.route('/rankings', methods=['GET'])
---
+++ config.py
@@ -0,0 +1,4 @@
+SQLALCHEMY_DATABASE_URI = 'mysql://<username>:<password>@<host>:<port>/<database_name>'
+SECRET_KEY = 'insert your own secret key'
+DEBUG = False
+SQLALCHEMY_ECHO = False
