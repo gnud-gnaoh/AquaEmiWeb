2a1848afdd284697a7970cc5cb167c9234019dce
gnud-gnaoh <hoangdung2.bubu@gmail.com>
2023-08-07 22:32:51 +0700

    Remove config from instance

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import pycountry
 import random
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_object('config')
+# app.config.from_object('config')
 app.config.from_pyfile('config.py')
 
 db.init_app(app)
